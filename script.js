a87770550eda3166aea0f5dbc4c05866d4074bb4
EdinsonPerez <perezea1010@gmail.com>
2023-10-18 14:39:07 -0300

    calculos

--- script.js
+++ script.js
@@ -58,3 +58,46 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error('Error al cargar los datos:', error);
         });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+  
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+    });
+  });
+
+  document.getElementById("quotation-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  
+    // Obtén los valores del formulario
+    const startDate = document.getElementById("start-date").value;
+    const endDate = document.getElementById("end-date").value;
+    const numPeople = parseInt(document.getElementById("num-people").value);
+    const selectedPackage = document.getElementById("selected-package").value;
+  
+    // Calcula el costo total
+    const costoTotal = calcularCostoTotal(startDate, endDate, numPeople, selectedPackage);
+  
+    // Muestra el resultado en la página
+    const quotationResult = document.getElementById("quotation-result");
+    quotationResult.innerHTML = "El costo total es: $" + costoTotal.toFixed(2);
+  });
+  
+  function calcularCostoTotal(startDate, endDate, numPeople, selectedPackage) {
+    const packagePrices = {
+      "Nauru": { price: 1999, days: 8, nights: 7 },
+      "Guadeloupe": { price: 2999, days: 10, nights: 8 },
+      "Bolivia": { price: 1599, days: 8, nights: 7 },
+      "South Korea": { price: 1000, days: 6, nights: 5 }
+    }
+  
+    const package = packagePrices[selectedPackage];
+    const numDays = Math.ceil((new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24));
+    const numNights = numDays - 1;
+    const totalPricePerPerson = package.price * (numDays / package.days) * numPeople;
+  
+    return totalPricePerPerson;
+  }
+  
